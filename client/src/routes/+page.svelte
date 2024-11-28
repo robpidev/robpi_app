d1bdeeb065cbf4091b4e8a50f58bb48419cb8f76
Rober Torres <robertdrakenfor@gmail.com>
2024-11-28 13:10:17 -0500

    First commit

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,80 @@
+<script>
+	import Links from './Links.svelte';
+</script>
+
+<main>
+	<section class="desctiption">
+		<div>
+			<img src="favicon.png" alt="robpi" />
+		</div>
+		<div class="info">
+			<span>Aprende sobre programación y mates.</span>
+			<Links />
+		</div>
+	</section>
+
+	<section class="section">
+		<h2>1. Linux</h2>
+		<p>Como usar el susbsitema de linux para windows y configuración</p>
+		<iframe
+			width="100%"
+			height="400px"
+			src="https://www.youtube.com/embed/t4XZLxAkOGU"
+			title="Windows Subsitem for LInux | Como usar linux sin salir de windows | WSL"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+		></iframe>
+	</section>
+</main>
+
+<style>
+	.desctiption {
+		padding: 1rem;
+		align-items: center;
+		display: flex;
+		max-width: 1200px;
+		gap: 1rem;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	main {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 1rem;
+		padding: 2rem;
+	}
+
+	.section {
+		background: rgba(255, 255, 255, 0.07);
+		max-width: 1200px;
+		width: 100%;
+		padding: 1rem;
+		border-radius: 12px;
+	}
+
+	.info {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.desctiption span {
+		/*color: var(--secondary);*/
+		font-size: 2rem;
+		font-weight: 600;
+		font-family: 'consolas';
+		justify-content: center;
+	}
+
+	h2 {
+		margin: 0;
+	}
+
+	iframe {
+		border-radius: 12px;
+	}
+</style>
---
+++ client/src/routes/Links.svelte
@@ -0,0 +1,75 @@
+<script>
+	import Icon from '@iconify/svelte';
+	const links = [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/robpidev',
+			bg: '#fff',
+			fg: 'gray',
+			icon: 'mdi:github'
+		},
+		{
+			name: 'Discord',
+			url: 'https://discord.gg/6kqy5H7',
+			bg: 'purple',
+			fg: 'white',
+			icon: 'ic:outline-discord'
+		},
+		{
+			name: 'YouTube',
+			url: 'https://youtube.com/robpi',
+			bg: 'red',
+			fg: 'white',
+			icon: 'mdi:youtube'
+		},
+		{
+			name: 'Telegram',
+			telegram: 'https://t.me/robpi',
+			bg: 'skyblue',
+			fg: '#000',
+			icon: 'mdi:telegram'
+		},
+		{
+			name: 'TikTok',
+			url: 'https://tiktok.com/@robpidev',
+			bg: 'black',
+			fg: 'white',
+			icon: 'ic:baseline-tiktok'
+		}
+	];
+</script>
+
+<ul>
+	{#each links as links}
+		<li>
+			<a style="background: {links.bg}; color: {links.fg};" href={links.url}>
+				<Icon icon={links.icon} style="font-size: 1.8rem;" />
+				{links.name}
+			</a>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		list-style: none;
+		gap: 1rem;
+		flex-wrap: wrap;
+		padding: 0;
+	}
+
+	a {
+		padding: 0.3rem 1rem 0.3rem 0.6rem;
+		border-radius: 8px;
+		text-decoration: none;
+		display: flex;
+		gap: 0.5rem;
+		align-items: center;
+	}
+
+	li:last-child a {
+		box-sizing: border-box;
+		border: 1px solid white;
+	}
+</style>
